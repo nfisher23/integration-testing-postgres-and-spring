f40c26a93a45ee16b019447d52f29cc6ee58c8c0
nick fisher <nick@nickolasfisher.com>
2018-12-01 12:36:36 -0800

    postgres-setup

---
+++ postgres-vm-sandbox/postgres-provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get -y install postgresql
+
+# set the default to listen to all addresses
+sudo sed -i "/port*/a listen_addresses = '*'" /etc/postgresql/10/main/postgresql.conf
+
+# allow any authentication mechanism from any client
+sudo sed -i "$ host all all all trust" /etc/postgresql/10/main/pg_hba.conf
+
+# create db named testdb
+sudo su postgres -c "createdb testdb"
+
+# restart the service to allow changes to take effect
+sudo service postgresql restart
